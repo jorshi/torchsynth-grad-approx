a1594e9c09cd4bfd90524ab61c5a360dd36abef3
Jordie Shier <jordieshier@gmail.com>
2021-12-18 14:39:53 -0800

    Cleaning up notebooks

--- NASH Experiment.py
+++ NASH Experiment.py
@@ -28,32 +28,36 @@ DURATION = 1.0
 
 # STFT function
 window_a = torch.tensor(hann(1024)).to("cuda")
+
+
 def stft_a(audio):
     x_stft = torch.stft(audio, n_fft=1024, window=window_a, return_complex=True)
-    x_mag = torch.sqrt(
-        torch.clamp((x_stft.real ** 2) + (x_stft.imag ** 2), min=1e-8)
-    )
+    x_mag = torch.sqrt(torch.clamp((x_stft.real ** 2) + (x_stft.imag ** 2), min=1e-8))
     return torch.log10(x_mag)
 
 
 # STFT function 2
 window_b = torch.tensor(hann(512)).to("cuda")
+
+
 def stft_b(audio):
     x_stft = torch.stft(audio, n_fft=512, window=window_b, return_complex=True)
-    x_mag = torch.sqrt(
-        torch.clamp((x_stft.real ** 2) + (x_stft.imag ** 2), min=1e-8)
-    )
+    x_mag = torch.sqrt(torch.clamp((x_stft.real ** 2) + (x_stft.imag ** 2), min=1e-8))
     return torch.log10(x_mag)
 
 
 # Load target
 sample_rate = 44100
 target, _ = librosa.load("./audio/snaredrum.wav", sr=sample_rate)
-#target = np.pad(target, [0, int(sample_rate * DURATION - len(target))])
+# target = np.pad(target, [0, int(sample_rate * DURATION - len(target))])
 ipd.Audio(target, rate=44100)
 
 # Configure Synth
-synth_config = SynthConfig(buffer_size_seconds=len(target)/sample_rate, batch_size=BATCH_SIZE, reproducible=False) 
+synth_config = SynthConfig(
+    buffer_size_seconds=len(target) / sample_rate,
+    batch_size=BATCH_SIZE,
+    reproducible=False,
+)
 voice = Voice(synth_config)
 # Run on the GPU if it's available
 if torch.cuda.is_available():
@@ -61,16 +65,20 @@ if torch.cuda.is_available():
 
 # Spectral loss A
 target_spectrogram_a = stft_a(torch.tensor(target).to("cuda"))
+
+
 def stft_loss_a(audio):
     spectrogram = stft_a(audio)
-    return torch.mean(torch.square(spectrogram - target_spectrogram_a), dim=(1,2))
+    return torch.mean(torch.square(spectrogram - target_spectrogram_a), dim=(1, 2))
 
 
 # Spectral loss B
 target_spectrogram_b = stft_b(torch.tensor(target).to("cuda"))
+
+
 def stft_loss_b(audio):
     spectrogram = stft_b(audio)
-    return torch.mean(torch.square(spectrogram - target_spectrogram_b), dim=(1,2))
+    return torch.mean(torch.square(spectrogram - target_spectrogram_b), dim=(1, 2))
 
 
 best_audio = None
@@ -84,7 +92,7 @@ for i in tqdm(range(25)):
         print(f"New best error: {sample_error}")
         min_error = sample_error
         best_audio = audio[min_sample].cpu().numpy()
-    
+
 
 ipd.Audio(best_audio, rate=sample_rate)
 
@@ -102,18 +110,20 @@ num_params = len(list(voice.parameters()))
 def fitness(individuals):
     # Update voice with new parameters
     parameters = [param for _, param in sorted(voice.named_parameters())]
-    new_values = torch.stack([torch.tensor(list(ind)) for ind in individuals], dim=1).to("cuda")
+    new_values = torch.stack(
+        [torch.tensor(list(ind)) for ind in individuals], dim=1
+    ).to("cuda")
     for i, parameter in enumerate(parameters):
         parameter.data = new_values[i]
-    
+
     audio, _, _ = voice()
     spectral_error_a = stft_loss_a(audio).cpu().numpy()
     spectral_error_b = stft_loss_b(audio).cpu().numpy()
-    
+
     error = []
     for i in range(len(audio)):
         error.append([spectral_error_a[i], spectral_error_b[i]])
-    
+
     return error
 
 
@@ -137,9 +147,7 @@ toolbox.register(
     num_params,
 )
 
-toolbox.register(
-    "population", tools.initRepeat, list, toolbox.individual
-)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 ref_points = tools.uniform_reference_points(num_objectives, 12)
 
@@ -159,11 +167,11 @@ toolbox.register("select", tools.selNSGA3, ref_points=ref_points)
 # +
 # Create the initial population
 pop = toolbox.population(n=BATCH_SIZE)
-#invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+# invalid_ind = [ind for ind in pop if not ind.fitness.valid]
 fitnesses = toolbox.evaluate(pop)
 for i, ind in enumerate(pop):
     ind.fitness.values = fitnesses[i]
-    
+
 logbook = tools.Logbook()
 
 stats = tools.Statistics(lambda ind: ind.fitness.values)
@@ -181,14 +189,14 @@ logbook.record(gen=0, evals=len(pop), **record)
 pbar = tqdm(range(1, 1000 + 1), desc="Generation 1")
 for gen in pbar:
     offspring = algorithms.varAnd(pop, toolbox, 0.5, 0.5)
-    
+
     fitnesses = toolbox.evaluate(offspring)
     for i, ind in enumerate(offspring):
         ind.fitness.values = fitnesses[i]
-        
+
     # Select the next generation population from parents and offspring
     pop = toolbox.select(pop + offspring, BATCH_SIZE)
-    
+
     # Compile statistics about the new population
     record = stats.compile(pop)
     logbook.record(gen=gen, evals=len(offspring), **record)
@@ -197,7 +205,9 @@ for gen in pbar:
 # +
 tools.selBest(pop, BATCH_SIZE)
 parameters = [param for _, param in sorted(voice.named_parameters())]
-new_values = torch.stack([torch.tensor(list(ind)) for ind in tools.selBest(pop, BATCH_SIZE)], dim=1).to("cuda")
+new_values = torch.stack(
+    [torch.tensor(list(ind)) for ind in tools.selBest(pop, BATCH_SIZE)], dim=1
+).to("cuda")
 for i, parameter in enumerate(parameters):
     parameter.data = new_values[i]
 
@@ -205,5 +215,3 @@ audio, _, _ = voice()
 # -
 
 ipd.Audio(audio[510].cpu().numpy(), rate=sample_rate)
-
-
--- SPSA.py
+++ SPSA.py
@@ -14,47 +14,124 @@
 
 import torch
 import torch.nn as nn
+from torch.autograd import gradcheck
 from torchsynth.config import SynthConfig
 from torchsynth.synth import AbstractSynth, Voice
 import IPython.display as ipd
+import librosa
+import matplotlib.pyplot as plt
 
-BATCH_SIZE = 128
+BATCH_SIZE = 1
 SR = 44100
 
+# Load target
+target, _ = librosa.load("./audio/snaredrum.wav", sr=SR)
+ipd.Audio(target, rate=SR)
+
 # Configure Synth
-synth_config = SynthConfig(batch_size=BATCH_SIZE, sample_rate=SR, reproducible=False) 
+synth_config = SynthConfig(
+    batch_size=BATCH_SIZE,
+    buffer_size_seconds=len(target) / SR,
+    sample_rate=SR,
+    reproducible=False,
+)
 voice = Voice(synth_config)
 # Run on the GPU if it's available
 if torch.cuda.is_available():
     voice = voice.to("cuda")
 
 
+class TorchSynthSPSA(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, synth):
+        ctx.save_for_backward(input)
+        ctx.synth = synth
+
+        parameters = [param for _, param in sorted(synth.named_parameters())]
+        for i, parameter in enumerate(parameters):
+            parameter.requires_grad = False
+            parameter.data = input[:, i]
+
+        output, _, _ = synth()
+
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        print("here in backward")
+        return None
+
+
 # +
+synth_func = TorchSynthSPSA.apply
+
+in_params = torch.rand(BATCH_SIZE, 78, requires_grad=True).to("cuda")
+audio = synth_func(in_params, voice)
+print(audio)
+ipd.Audio(audio[0].detach().cpu().numpy(), rate=SR)
+
+
+# -
+
+
 class TorchSynth(nn.Module):
-    
+    """
+    Module that contains randomly initialized synth paramaters
+    """
+
     def __init__(self, synth: AbstractSynth):
         super(TorchSynth, self).__init__()
         self.synth = synth
-        self.parameters = [param for _, param in sorted(self.synth.named_parameters())]
-        
-    
-    def forward(self, input):
+        self.num_parameters = len(list(synth.parameters()))
+        self.batch_size = synth.batch_size
+
+        # These are parameters for the synth
+        self.synth_parameters = nn.Parameter(
+            torch.empty(self.batch_size, self.num_parameters)
+        )
+        nn.init.uniform_(self.synth_parameters)
+
+    def forward(self):
         """
         Input is a set of parameters for the synth
         """
-        for i, parameter in enumerate(self.parameters):
-            parameter.data = input[:,i]
+        return TorchSynthSPSA.apply(self.synth_parameters, self.synth)
 
-        return self.synth()
-        
 
-    
+synth = TorchSynth(voice).to("cuda")
+
+audio = synth()
+ipd.Audio(audio[0].detach().cpu().numpy(), rate=SR)
+
+# +
+## Try optimizing to find parameters for a target
+
+# +
+# STFT function
+window = torch.hann_window(1024).to("cuda")
+
+
+def stft(audio):
+    x_stft = torch.stft(audio, n_fft=1024, window=window, return_complex=True)
+    x_mag = torch.sqrt(torch.clamp((x_stft.real ** 2) + (x_stft.imag ** 2), min=1e-8))
+    return torch.log10(x_mag)
+
+
+# Spectral loss A
+target_spectrogram = stft(torch.tensor(target).to("cuda"))
+
+
+def stft_loss(audio):
+    spectrogram = stft(audio)
+    return torch.mean(torch.square(spectrogram - target_spectrogram), dim=(1, 2))
+
+
 # -
 
-synth = TorchSynth(voice)
+plt.imshow(target_spectrogram.cpu().numpy(), aspect="auto", origin="lower")
 
-audio, params, _ = synth(torch.rand(128, 78).to("cuda"))
+output_spec = stft(audio)
+plt.imshow(output_spec[0].detach().cpu().numpy(), aspect="auto", origin="lower")
 
-ipd.Audio(audio[0].cpu().numpy(), rate=SR)
-
-
+error = stft_loss(audio)
+error.backward()
